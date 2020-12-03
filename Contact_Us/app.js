2c5aaa4fcc9b8ac0c4eae791d69b6a6b8b375ec6
Xitiz Verma <xitizverma@gmail.com>
2020-12-03 18:23:46 +0530

    adding mongoose connection to MongoDB Atlas

---
+++ Contact_Us/app.js
@@ -0,0 +1,40 @@
+const express = require("express"); 
+const app = express(); 
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ 
+	extended:true
+})); 
+
+mongoose.connect("mongodb+srv://Avacado:Avacado@cluster0.bu2ko.mongodb.net/testDB",
+{
+    
+    useNewUrlParser : true,
+    useUnifiedTopology :true ,
+});
+//create a new data schema
+const testSchema = {
+    email : String ,
+    number : Number ,
+    description : String 
+}
+const Note = mongoose.model("Note",testSchema);
+
+app.get("/", function(req, res) { 
+    res.sendFile(__dirname + "/Contact Us.html"); 
+}); 
+
+app.post("/", function(req, res) { 
+    let newNote = new Note ({
+         email : req.body.email,
+         number : req.body.number, 
+         description : req.body.description
+        });
+        newNote.save();
+        res.redirect('/');
+}); 
+
+app.listen(3000,function(){
+    console.log("Server is running on port 3000");
+})
